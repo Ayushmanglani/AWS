a34b758e337f0035be3e25af2bedccf5b87208f7
Ayush_manglani <ayush.manglani@gmail.com>
2020-06-02 23:14:23 +0530

    S3-Lambda-SNS

---
+++ Lambda/s3_sns.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+
+import json
+import urllib
+import boto3
+
+from uuid import uuid4
+def lambda_handler(event, context):
+    s3 = boto3.client("s3")
+    # bucket = event['Records'][0]['s3']['bucket']['name']
+    sns = boto3.client('sns')
+    sns.publish(
+        TopicArn='arn:aws:sns:us-east-1:298671723150:Default_CloudWatch_Alarms_Topic',
+        Subject='Object Upload',
+        Message='Object Uploaded in s3 successfully in the Bucket lambdasnsbucket'
+    )
